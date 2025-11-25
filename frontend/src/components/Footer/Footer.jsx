ae59766a63053af9c87f64a2404b40e8f67e2796
Ajay Veer Singh <avisingh.veer@gmail.com>
2025-11-26 00:33:14 +0530

    footer updated

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 export default function Footer(){
   return (
     <footer className={styles.wrap}>
-      <div className={styles.inner}>© {new Date().getFullYear()} FlowWithYogaSisters • Built with ❤️ and by Dakshaj's Love ❤️</div>
+      <div className={styles.inner}>© {new Date().getFullYear()} FlowWithYogaSisters • Built with ❤️</div>
     </footer>
   )
 }
